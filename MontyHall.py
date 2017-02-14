54a6540afbe82cdc7a4fa07a381f3bb858e33045
Philip Ni <philipni122@gmail.com>
2017-02-14 14:59:46 -0500

    Final touches; now able to play game yourself
    Added static methods for easier external/shell calls to the game

--- MontyHall.py
+++ MontyHall.py
@@ -9,12 +9,14 @@ class GameShow:
     def __init__(self):
         self.doors = ['reward', 'nothing a', 'nothing b']
         self.reward = 0
+        self.wrong = 0
         self.host = ''
         self.player = ''
         self.set_door()
 
     def set_door(self):
         rnd.shuffle(self.doors)
+        print(self.doors)
         self.reward = self.doors.index('reward')
         self.host = 'reward'
 
@@ -23,21 +25,19 @@ class GameShow:
         decision = input('Enter (1, 2, or 3): ')
 
         # loop if invalid input
-        while decision != 3 and decision != 2 and decision != 1:
+        while decision != '3' and decision != '2' and decision != '1':
             decision = input('Enter (1, 2, or 3): ')
             
-        self.player = self.doors[decision]
-#         self.doors.remove(self.player)
+        self.player = self.doors[int(decision) - 1]
 
     def set_wrong(self):       # this method is suboptimal
         # pick a wrong door that has not been chosen, yet
         while self.host == 'reward' or self.host == self.player:
             self.host = rnd.choice(self.doors)
-        
-#         self.doors.remove(self.host)
+            self.wrong = self.doors.index(self.host) + 1
         
     def get_wrong(self):
-        return self.host
+        return self.wrong
 
     def check_win(self):
         if self.player == 'reward':
@@ -64,7 +64,7 @@ class GameShow:
     def play(self):
         again = 'y'
 
-        while again.lower() is 'y':
+        while again.lower() == 'y':
             again = ''
 
             print("There are three doors. Only one of these doors has a reward behind it. Choose wisely...")
@@ -72,23 +72,27 @@ class GameShow:
             self.player_choice()
 
             self.set_wrong()
-            print("Here is a clue. There is no reward behind Door", self.get_wrong())
-            change = input("Do you want to change? (Y/N) ")
+            print("\nHere is a clue. There is no reward behind Door", self.get_wrong(), '\b.')
+            change = input("\nDo you want to change? (Y/N) ")
 
             # loop if invalid input
-            while change.lower() != 'y' or change.lower() != 'n':
+            while change.lower() != 'y' and change.lower() != 'n':
                 change = input("Do you want to change? (Y/N) ")
 
-            if change is 'y':
-                print("Host: ", self.host, "\tPlayer: ", self.player)
+            # Uncomment below to cheat
+            # print("\nHost: ", self.wrong + 1, '(', self.host, ')', "\tPlayer: ", self.player)
+            
+            if change == 'y':
                 self.player_choice()
 
-            print("Door:", self.reward, ", Choice:", self.player)
+            print("\nReward:", self.reward + 1, ", Choice:", self.doors.index(self.player) + 1, self.player)
             self.check_win()
 
             # loop if invalid input
-            while again.lower() != 'y' or again.lower() != 'n':
-                again = input("Play again? (Y/N) ")
+            while again.lower() != 'y' and again.lower() != 'n':
+                again = input("\nPlay again? (Y/N) ")
+            
+            print('\n\n')
 
         print("Wins:", self.win_count, "/", self.total)
         print("Losses:", self.lose_count, "/", self.total)
@@ -98,13 +102,12 @@ class GameShow:
             self.set_door()     # reset door placement
             self.player = rnd.choice(self.doors)
             self.set_wrong()
+            self.get_wrong()
 
             if method == 2:
-                wrong = self.get_wrong()
-                self.choice = self.rand_change()
+                self.rand_change()
             elif method == 1:
-                wrong = self.get_wrong()
-                self.choice = self.always_change()
+                self.always_change()
             # keep choice if "no change" method (0)
 
             # print(x, "\b:", self.get_door(), ",", self.get_choice())
@@ -112,14 +115,27 @@ class GameShow:
 
         print("Wins:", self.win_count, "/", self.total)
         print("Losses:", self.lose_count, "/", self.total)
+    
+    @staticmethod
+    def auto(method=0, trials=100):
+        g = GameShow()
+        g.autoplay(method, trials)
+        
+    @staticmethod
+    def game()
+        g = GameShow()
+        g.play()
 
 
-print("No Change")
-g = GameShow()
-g.autoplay(0, 1000000)
-print("Always Change")
-g = GameShow()
-g.autoplay(1, 1000000)
-print("Randomly Change")
-g = GameShow()
-g.autoplay(2, 1000000)
+# print("No Change")
+# g = GameShow()
+# g.autoplay(0, 1000000)
+# print("Always Change")
+# g = GameShow()
+# g.autoplay(1, 1000000)
+# print("Randomly Change")
+# g = GameShow()
+# g.autoplay(2, 1000000)
+
+# g = GameShow()
+# g.play()
